2dc28f529fdd5abf7ab74a11a0ddfdafdf245c8a
tazeek <tazeek.rakib@gmail.com>
2022-12-05 18:40:15 +0600

    Attributes added

--- food_recipe.py
+++ food_recipe.py
@@ -4,4 +4,8 @@ class Food_Recipe():
 
     def __init__(self):
 
-        ...
+        self._id = None
+        self._hyperlink = None
+        self._name = None
+        self._keywords = None
+        self._cost = None
