b19ea46f083e7fddbf33b20689f7559f3e1b1608
tazeek <tazeek.rakib@gmail.com>
2022-12-05 18:52:08 +0600

    User attributes added

--- user.py
+++ user.py
@@ -4,4 +4,21 @@ class User():
 
     def __init__(self):
 
+        self._email = None
+        self._password = None
+
+        self._name = None
+        self._recipes_list = None
+
+    def authenticate_user(self):
+
         ...
+
+    def fetch_recipes(self):
+
+        ...
+
+    def update_user_info(self):
+
+        ...
+
